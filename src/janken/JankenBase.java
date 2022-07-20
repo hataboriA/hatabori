416eec4a2f3885c955e2ead644d26ceceafc9260
XW-000392\a-hatabori <a-hatabori@xware.co.jp>
2022-07-20 10:28:51 +0900

    push test

--- src/janken/JankenBase.java
+++ src/janken/JankenBase.java
@@ -7,6 +7,7 @@ public class JankenBase {
 	private String[] hands = { G, T, P };
 	
 	public String getHandtypeG(){
+		System.out.println("てすと");
 		return this.G;
 	}
 	public String getHandtypeT(){
